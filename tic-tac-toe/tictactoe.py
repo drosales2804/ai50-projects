495572fd45092a432f0fcc62e702c071b13b69cb
drosales2804 <greycos_drag2@hotmail.com>
2025-01-09 10:10:43 -0600

    Corrección de la logica de ganador X en consecutivo

--- tic-tac-toe/tictactoe.py
+++ tic-tac-toe/tictactoe.py
@@ -75,17 +75,17 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     for row in board:
-        if row[0] == row[1] == row[2]:
-            return row[0]
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]  # <- Es importante que también se verifique que `row[0]` no sea `None`
 
     for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col]:
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] is not None:
             return board[0][col]
 
-    if board[0][0] == board[1][1] == board[2][2]:
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
         return board[0][0]
 
-    if board[0][2] == board[1][1] == board[2][0]:
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
         return board[0][2]
 
     return None
